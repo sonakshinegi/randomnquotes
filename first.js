fa38051ec682fb6700536067b5d566a8a711c410
Sonakshi Negi <sonakshinegi21@gmail.com>
2022-10-07 10:48:31 +0530

    changes

--- first.js
+++ first.js
@@ -1,41 +1,27 @@
 const generateQuote = function() {
     const quotes = [
     {
-        quote: "Do not pity the dead, Harry. Pity the living, and, above all those who live without love.",
-        author: "Albus Dumbledore"
+        quote: "Life is what happens when you’re busy making other plans.",
+        author: "John Lennon"
     },
     {
-        quote: "It is impossible to manufacture or imitate love",
-        author: "Horace Slughorn"
+        quote: "You must be the change you wish to see in the world.",
+        author: "Mahantma Gandhi"
     },
     {
-        quote: "Being different isn't a bad thing. I means that you are brave enough to be yourself.",
-        author: "Luna Lovegood"
+        quote: "Get busy living or get busy dying.",
+        author: "Stephen King "
     },
     {
-        quote: "If you want to know what a man’s like, take a good look at how he treats his inferiors, not his equals.",
-        author: "Sirius Black"
+        quote: "If you’re going through hell, keep going.",
+        author: "Winston Churchill"
     },
     {
-        quote: "Never trust anything that can think for itself if you can’t see where it keeps its brain.",
-        author: "Arthur Weasley"
+        quote: "Those who dare to fail miserably can achieve greatly.",
+        author: "John F. Kennedy"
     },
-    {
-        quote: "Every human life is worth the same, and worth saving.",
-        author: "Kingsley Shacklebolt"
-    },
-    {
-        quote: "Have a biscuit, Potter.",
-        author: "Minerva McGonagall"
-    },
-    {
-        quote: "Happiness can be found even in the darkest of times, if one only remembers to turn on the light.",
-        author: "Albus Dumbledore"
-    },
-    {
-        quote: "Socks are Dobby’s favorite, favorite clothes, sir!",
-        author: "Dobby"
-    }
+    
+    
 ];
 
     let arrayIndex = Math.floor(Math.random() * quotes.length);
